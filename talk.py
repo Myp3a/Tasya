715854bec1a6bc2471749bd99b16b9c670361479
Myp3a <murza8965@gmail.com>
2023-05-11 01:10:35 +0300

    Прямой запрос перевода вместо модуля

--- talk.py
+++ talk.py
@@ -3,14 +3,17 @@ import config
 import re
 from deep_translator import DeeplTranslator
 
-def translate(text,src=None,dst=None):
+async def translate(text,src=None,dst=None):
+    async with aiohttp.ClientSession() as sess:
+        async with sess.post("https://api-free.deepl.com/v2/translate", data={"target_lang":dst,"source_lang":src,"text":text}, headers={"Authorization": f"DeepL-Auth-Key {config.deepl_key}"}) as resp:
+            return (await resp.json())["translations"][0]["text"]
     return DeeplTranslator(api_key=config.deepl_key, source=src, target=dst).translate(text)
 
 def fix_name(text):
     return re.sub(r'Tas.a',"Tasya",text)
 
 async def generate(chardef, exdialog, history):
-    history_en = fix_name(translate(history,"ru","en"))
+    history_en = fix_name(await translate(history,"ru","en"))
 
     prompt = chardef + exdialog + history_en + "\nTasya:"
 
@@ -19,4 +22,4 @@ async def generate(chardef, exdialog, history):
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=600)) as session:
         async with session.post("http://"+config.pygip+":"+config.pygport+"/api/v1/generate", json=req_json) as resp:
             text = fix_name((await resp.json())['results'][0]['text'])
-            return translate(text,"en","ru")
+            return await translate(text,"en","ru")
