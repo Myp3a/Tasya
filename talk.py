09944c226e8365c68333a7231b0d7a06b2672880
Myp3a <murza8965@gmail.com>
2023-05-11 01:01:19 +0300

    Новая версия API, DeepL вместо Google

--- talk.py
+++ talk.py
@@ -1,29 +1,22 @@
 import aiohttp
 import config
 import re
-from deep_translator import GoogleTranslator
+from deep_translator import DeeplTranslator
 
 def translate(text,src=None,dst=None):
-    return GoogleTranslator(source=src, target=dst).translate(text)
+    return DeeplTranslator(api_key=config.deepl_key, source=src, target=dst).translate(text)
+
+def fix_name(text):
+    return re.sub(r'Tas.a',"Tasya",text)
 
 async def generate(chardef, exdialog, history):
-    history_en = translate(history,"ru","en")
-    # inp = input("You: ")
-    # history += "You: " + inp
+    history_en = fix_name(translate(history,"ru","en"))
+
     prompt = chardef + exdialog + history_en + "\nTasya:"
-    # print(prompt)
-    model_data = [50,True,0.5,0.9,1,1.1,0,0,0,1,0,1,True,"You","Tasya","",True,1500,1]
-    req_json = {"data":[prompt,] + model_data}
+
+    req_json = {"prompt":prompt, 'early_stopping': True}
+    print(prompt)
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=600)) as session:
-        async with session.post("http://"+config.pygip+":"+config.pygport+"/run/textgen", json=req_json) as resp:
-            print("---")
-            print((await resp.json())["data"][0][-200:])
-            print("---")
-            said = re.findall(r'(?:Tasya:)(.*)', (await resp.json())["data"][0])
-            # print(said[-1])
-            return translate(said[-1],"en","ru")
-            # print("\nTasya: " + said[-1] + "\n")
-            # history += "\nTasya: " + said[-1] + "\n"
-    # print("***")
-    # print(history)
-    # print("***")
+        async with session.post("http://"+config.pygip+":"+config.pygport+"/api/v1/generate", json=req_json) as resp:
+            text = fix_name((await resp.json())['results'][0]['text'])
+            return translate(text,"en","ru")
