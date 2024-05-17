9ccbdb2c01181cb2f239df000639a6718756f8f6
Myp3a <murza8965@gmail.com>
2024-05-17 15:19:01 +0300

    feat: use RAG-assisted endpoint

--- talk.py
+++ talk.py
@@ -17,24 +17,26 @@ async def translate(text,src=None,dst=None):
 def fix_name(text):
     return re.sub(r'Tas.a',"Tasya",text)
 
-async def generate(prompt, history, lock):
+def fix_pipes(text):
+    return re.sub(r"\|\|", "|", text)
+
+# Direct LLaMA generation
+async def generate_old(prompt, history, lock):
     try:
         history_en = fix_name(await translate(history,"ru","en"))
     except:
         logger.error("Translation to english failed")
         return "Иди нахер, животное. Я отдыхаю."
 
-    full_prompt = prompt + history_en + "\n<|model|>:"
+    full_prompt = prompt + history_en + "\n<|start_header_id|>model<|end_header_id|>: "
 
-    req_json = {"prompt":full_prompt, "max_context_length":2048, "max_length":150, "quiet": True, "use_world_info": True}
+    req_json = {"prompt":full_prompt, "model": "llama3", "raw": True, "stream": False}
     try:
         async with lock:
             logger.info("Asking Pyg for response")
             async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=600)) as session:
-                async with session.post("http://"+config.pygip+":"+config.pygport+"/api/v1/generate", json=req_json) as resp:
-                    text = fix_name((await resp.json())['results'][0]['text']).replace("||","|")
-                    text = text.split("<|user|>")[0]
-                    text = text.replace("<|model|>:","")
+                async with session.post("http://"+config.ollama_ip+":"+config.ollama_port+"/api/generate", json=req_json) as resp:
+                    text = (await resp.json())["response"]
                     logger.info(f"Pyg resp: {text}")
     except:
         logger.error("Pyg error")
@@ -44,5 +46,28 @@ async def generate(prompt, history, lock):
     except:
         logger.error("Translation to russian failed")
         return "Иди нахер, животное. Я отдыхаю."
-        
+
+# RAG wrapper
+async def generate(prompt, history, lock):
+    try:
+        history_en = fix_pipes(fix_name(await translate(history,"ru","en")))
+    except:
+        logger.error("Translation to english failed")
+        return "Иди нахер, животное. Я отдыхаю."
     
+    req_json = {"history":history_en}
+    try:
+        async with lock:
+            logger.info("Asking Pyg for response")
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=600)) as session:
+                async with session.post("http://localhost:8085/text_input", json=req_json) as resp:
+                    text = (await resp.json())["text"]
+                    logger.info(f"Pyg resp: {text}")
+    except:
+        logger.error("Pyg error")
+        return "Иди нахер, животное. Я отдыхаю."
+    try:
+        return await translate(text,"en","ru")
+    except:
+        logger.error("Translation to russian failed")
+        return "Иди нахер, животное. Я отдыхаю."
